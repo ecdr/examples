13dbc287bcdb97a15ae8b2e6d6e6509bfa63e5b6
ecdr <Github.0hv@gishpuppy.com>
2013-03-24 15:13:21 -0800

    PIO - fix button poll, peek, add second led to example

--- pio/buttonled.lua
+++ pio/buttonled.lua
@@ -6,22 +6,22 @@
 -- Polling - need to call poll periodically
 -- Debounce - after a change it ignores further changes for a few (clockdebounce) calls
 --
--- Button is assumed to connect to ground (so 1 on pin indicates no press)
--- Button states reported as 1 for pressed, 0 for not pressed (reverse of pin state)
+-- Button is assumed to connect to ground, with no external pullup (so 1 on pin indicates no press)
+-- Button states reported as true for pressed, false for not pressed
 
 do
   local UNPRESSED = 1
   local PRESSED = 0
   local clockdebounce = 3
 
--- Create button object on given PIO pin
+-- Create button on given PIO pin
   local function new(pin) 
     pio.pin.setdir( pio.INPUT, pin )
     pio.pin.setpull( pio.PULLUP, pin )
     return {pin=pin, state=UNPRESSED, clock=clockdebounce, fpressed = false }
   end
 
---  local function peek() return not pio.pin.getval( pin ) end
+--  local function peek(but) return (pio.pin.getval( but.pin ) == PRESSED) end
 
 -- Poll current state of button
   local function poll(but) 
@@ -38,7 +38,7 @@ do
 		but.clock = 0
 	   end
 	end
-      return not but.state
+      return (but.state == PRESSED)
   end
 
 -- Report if button pressed since last call to pressed
@@ -62,7 +62,8 @@ end	-- button
 
 
 -- 
-local uartid, invert, ledpin = 0, false
+local uartid, invert = 0, false
+local ledpin, ledpin2
 local button1pin, button2pin
 local light = false
 
@@ -72,6 +73,7 @@ if pd.board() == "EK-LM3S8962" then
   button2pin = pio.PE_1
 elseif pd.board() == "EK-LM4F120" then
   ledpin = pio.PF_1			-- Red is PF 1, Blue is PF 2, Green is PF 3
+  ledpin2 = pio.PF_2
   button1pin = pio.PF_4
   button2pin = pio.PF_0			-- PF_0 Requires special handling to use
 else
@@ -79,27 +81,32 @@ else
   return
 end
 
-function setled( bled )
+-- Turn LED on lpin on if bled true, else turn off
+function setled( lpin, bled )
   if bled and not invert then
-    pio.pin.sethigh( ledpin )
+    pio.pin.sethigh( lpin )
   else
-    pio.pin.setlow( ledpin )
+    pio.pin.setlow( lpin )
   end
 end
  
 pio.pin.setdir( pio.OUTPUT, ledpin )
+pio.pin.setdir( pio.OUTPUT, ledpin2 )
+
 
 print( "Hello from eLua on " .. pd.board() )
 print "Lightswitch, press button to turn LED on or off"
+print "Second LED is on while button pressed"
 print "Press any key to end this demo.\n"
 
 
 local b1 = button.new(button1pin)
 while uart.getchar( uartid, 0 ) == "" do
+  if ledpin2 then setled( ledpin2, button.poll(b1)) end
+  tmr.delay( 0, 10000 )
   if button.pressed(b1) then
 	light = not light
-	setled( light )
+	setled( ledpin, light )
   end
-  tmr.delay( 0, 10000 )
 end	
 
